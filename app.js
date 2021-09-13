5e6cbf233810f3605d3d0a2ac37da4064e9c1e67
sergio-canovas <sergicano8@gmail.com>
2021-09-13 18:37:24 +0200

    arreglo

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const fs= require("fs");
 
 let salida = "";
 const crearArchivo = async (base)=>{
-    for(let i = 0; i<=10;i++){
-        salida+=`${base} x ${i} = ${base * i}\n`
+    try{
+        for(let i = 0; i<=10;i++){
+            salida+=`${base} x ${i} = ${base * i}\n`
+        }
+        console.log(salida);
+        fs.writeFileSync(`tabla-${base}.txt`, salida);
+        console.log("Fichero creado");
     }
-    console.log(salida);
-    fs.writeFileSync(`tabla-${base}.txt`, salida);
-    console.log("Fichero creado");
+    catch(err) {
+        throw (err);
+    }
 }
 //export
 module.exports = {
